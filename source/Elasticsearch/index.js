5f9d4b08bab4954b5fa0751fd7e6b86730aae20d
AzizTafougalti <aziz.walker19@gmail.com>
2018-09-27 12:36:20 +0100

    Commit

--- source/Elasticsearch/index.js
+++ source/Elasticsearch/index.js
@@ -1,7 +1,7 @@
 import Entity from '../index'
-import { default as Elasticsearch } from '@oudyworks/drivers/ElasticSearch'
+import ElasticSearchBatch from '@oudyworks/drivers/ElasticSearch/Batch'
 
-class ElasticsearchEntity extends Entity {
+class ElasticSearchEntity extends Entity {
     bind(state, trackChange = true, bindObject = {}) {
         if (state._id) {
             state.id = state._id
@@ -9,68 +9,58 @@ class ElasticsearchEntity extends Entity {
         }
         return super.bind(state, trackChange, bindObject)
     }
-    elasticsearchDocument() {
+    elasticSearchDocument() {
         let document = JSON.parse(this.json())
         delete document.id
         return document
     }
     save(bind) {
 
-        let payload = this.elasticsearchDocument(),
+        let payload = this.elasticSearchDocument(),
             context = this[Entity.context],
-            client = this.constructor[ElasticsearchEntity.client](context),
-            index = this.constructor[ElasticsearchEntity.index](context),
-            type = this.constructor[ElasticsearchEntity._type](context),
+            client = this.constructor[ElasticSearchEntity.client](context),
+            index = this.constructor[ElasticSearchEntity._index](context),
+            type = this.constructor[ElasticSearchEntity._type](context),
             $return = Promise.resolve(this)
 
         if (this.id)
-            $return = Elasticsearch.getClient(client).update({
-                index,
-                type,
-                id: this.id,
-                body: {
-                    doc: bind.newObject
-                }
-            }).then(
-                () => {
-                    if (bind) {
-                        this.emit(
-                            'update',
-                            bind
-                        )
-                        this.constructor.emit(
-                            'update',
-                            bind
-                        )
+            $return = ElasticSearchBatch.update(this.id, payload, client, index, type)
+                .then(
+                    () => {
+                        if (bind) {
+                            this.emit(
+                                'update',
+                                bind
+                            )
+                            this.constructor.emit(
+                                'update',
+                                bind
+                            )
+                        }
                     }
-                }
-            )
-
+                )
         else
-            $return = Elasticsearch.getClient(client).index({
-                index,
-                type,
-                body: payload
-            }).then(
-                ({ _id }) => {
-                    this.id = _id
-                    if (bind)
-                        bind.id = this.id
-                }
-            ).then(
-                () => {
-                    if (bind) {
-                        this.emit(
-                            'new',
-                            bind
-                        )
-                        this.constructor.emit(
-                            'new',
-                            bind
-                        )
+            $return = ElasticSearchBatch.insert(payload, client, index, type)
+                .then(
+                    id => {
+                        this.id = id
+                        if (bind)
+                            bind.id = this.id
                     }
-                }
-            )
+                ).then(
+                    () => {
+                        if (bind) {
+                            this.emit(
+                                'new',
+                                bind
+                            )
+                            this.constructor.emit(
+                                'new',
+                                bind
+                            )
+                        }
+                    }
+                )
 
         return $return.then(
             () => {
@@ -85,26 +75,25 @@ class ElasticsearchEntity extends Entity {
                         bind
                     )
                 }
-                return this
+                return this.constructor.clear(this.id, context).then(
+                    () =>
+                        this
+                )
             }
         )
 
     }
     static load(id, context = {}) {
-        if (!id) {
-            return Promise.resolve(new this())
-        }
         return this[Entity.validateContext](context).then(
             () => {
-                let client = this[ElasticsearchEntity.client](context),
-                    index = this[ElasticsearchEntity.index](context),
-                    type = this[ElasticsearchEntity._type](context)
-                return Elasticsearch.getClient(client).get({ index, type, id })
-                    .then(object => {
+                let client = this[ElasticSearchEntity.client](context),
+                    index = this[ElasticSearchEntity._index](context),
+                    type = this[ElasticSearchEntity._type](context),
+                    cache = this[ElasticSearchEntity.cache](context)
+                return ElasticSearchBatch.load(id, client, index, type, cache)
+                    .then(document => {
                         let instance = new this()
-                        instance[Entity.context] = context
-                        object._source.id = object._id
-                        instance.bind(object._source || {}, false)
+                        instance.bind(document || {}, false)
                         return instance
                     })
             }
@@ -113,96 +102,83 @@ class ElasticsearchEntity extends Entity {
     static loadAll(query = { query: { match_all: {} }, limit: 20, page: 1 }, context = {}) {
         return this[Entity.validateContext](context).then(
             () => {
-                let client = this[ElasticsearchEntity.client](context),
-                    index = this[ElasticsearchEntity.index](context),
-                    type = this[ElasticsearchEntity._type](context)
-                return Elasticsearch.getClient(client).search({
-                    index,
-                    type,
-                    size: query.limit,
-                    from: query.limit * (query.page - 1),
-                    body: {
-                        query: query.query
-                    }
-                }).then(
-                    result => {
-                        return {
-                            list: result.hits.hits.map(
-                                document => {
-                                    let instance = new this()
-                                    instance[Entity.context] = context
-                                    document._source.id = document._id
-                                    instance.bind(document._source || {}, false)
-                                    return instance
-                                }
-                            ),
-                            total: result.hits.total,
-                            page: query.page,
-                            limit: query.limit
-                        }
-                    }
-                )
+                let client = this[ElasticSearchEntity.client](context),
+                    index = this[ElasticSearchEntity._index](context),
+                    type = this[ElasticSearchEntity._type](context),
+                    cache = this[ElasticSearchEntity.cache](context)
+                return ElasticSearchBatch.loadAll(query, client, index, type, cache)
+                    .then(result => {
+                        result.list = result.list.map(document => {
+                            let instance = new this()
+                            instance.bind(document || {}, false)
+                            return instance
+                        })
+                        return result
+                    })
             }
         )
     }
     static loadMany(ids, context = {}) {
         return this[Entity.validateContext](context).then(
             () => {
-                let client = this[ElasticsearchEntity.client](context),
-                    index = this[ElasticsearchEntity.index](context),
-                    type = this[ElasticsearchEntity._type](context)
-                return Elasticsearch.getClient(client).mget({
-                    index,
-                    type,
-                    body: {
-                        ids
-                    }
-                }).then(({ docs }) => {
-                    return docs.map(object => {
-                        let instance = new this()
-                        instance[Entity.context] = context
-                        object._source.id = object._id
-                        instance.bind(object._source || {}, false)
-                        return instance
-                    })
-                })
+                let client = this[ElasticSearchEntity.client](context),
+                    index = this[ElasticSearchEntity._index](context),
+                    type = this[ElasticSearchEntity._type](context),
+                    cache = this[ElasticSearchEntity.cache](context)
+                return ElasticSearchBatch.loadMany(ids, client, index, type, cache)
+                    .then(result =>
+                        result.map(document => {
+                            let instance = new this()
+                            instance.bind(document || {}, false)
+                            return instance
+                        }))
             }
         )
     }
     static count(query = { match_all: {} }, context = {}) {
         return this[Entity.validateContext](context).then(
             () => {
-                let client = this[ElasticsearchEntity.client](context),
-                    index = this[ElasticsearchEntity.index](context),
-                    type = this[ElasticsearchEntity._type](context)
-                return Elasticsearch.getClient(client).count({
-                    index,
-                    type,
-                    body: {
-                        query
-                    }
-                }).then(({ count }) => count)
+                let client = this[ElasticSearchEntity.client](context),
+                    index = this[ElasticSearchEntity._index](context),
+                    type = this[ElasticSearchEntity._type](context),
+                    cache = this[ElasticSearchEntity.cache](context)
+                return ElasticSearchBatch.count(query, client, index, type, cache)
+            }
+        )
+    }
+    static clear(id, context = {}) {
+        return this[Entity.validateContext](context).then(
+            () => {
+                let client = this[ElasticSearchEntity.client](context),
+                    index = this[ElasticSearchEntity._index](context),
+                    type = this[ElasticSearchEntity._type](context),
+                    cache = this[ElasticSearchEntity.cache](context)
+                return ElasticSearchBatch.clear(id, client, index, type, cache)
             }
         )
     }
 }
 
-ElasticsearchEntity.client = Symbol('client')
-ElasticsearchEntity.index = Symbol('index')
-ElasticsearchEntity._type = Symbol('type')
+ElasticSearchEntity.client = Symbol('client')
+ElasticSearchEntity._index = Symbol('index')
+ElasticSearchEntity._type = Symbol('type')
+ElasticSearchEntity.cache = Symbol('cache')
 
-ElasticsearchEntity[ElasticsearchEntity.client] = function (context) {
+ElasticSearchEntity[ElasticSearchEntity.client] = function (context) {
     return 'default'
 }
-ElasticsearchEntity[ElasticsearchEntity._type] = function (context) {
+ElasticSearchEntity[ElasticSearchEntity._type] = function (context) {
     return '_doc'
 }
-ElasticsearchEntity[ElasticsearchEntity.index] = function (context) {
+ElasticSearchEntity[ElasticSearchEntity._index] = function (context) {
     return this[Entity.context].map(
         key =>
             `${key}:${context[key]}`
 
     ).concat(this.pluralName.toLowerCase()).join(':')
 }
+ElasticSearchEntity[ElasticSearchEntity.cache] = function (context) {
+    return false
+}
 
-export default ElasticsearchEntity
+export default ElasticSearchEntity
